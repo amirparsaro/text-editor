36ba4176aeb18352b34dcbb7bb39d18011b8a844
amirparsaro <amirparsarouhani@gmail.com>
2025-04-24 22:29:04 +0330

    modify methods openFile, saveFile, newFile

--- src/editor/FileManager.java
+++ src/editor/FileManager.java
@@ -1,29 +1,61 @@
 package editor;
 
 import javax.swing.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.PrintStream;
+import java.util.Scanner;
 
 public class FileManager {
     public static void openFile(TextEditor textEditor, JTextArea textArea) {
         JFileChooser fileChooser = new JFileChooser();
         if (fileChooser.showOpenDialog(textEditor) == JFileChooser.APPROVE_OPTION) {
-            //File file = fileChooser.getSelectedFile();
-            //TODO
+            File file = fileChooser.getSelectedFile();
+            Scanner scanner = null;
+            try {
+                scanner = new Scanner(file);
+            } catch (FileNotFoundException e) {
+                System.out.println("Cannot find file");
+                return;
+            }
+
+            String content = "";
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                content += line + "\n";
+            }
+
+            textArea.setText(content);
         }
     }
 
     public static void saveFile(TextEditor textEditor, JTextArea textArea) {
-        if (textEditor.currentFile == null){
+        if (textEditor.currentFile == null) {
             JFileChooser fileChooser = new JFileChooser();
             if (fileChooser.showSaveDialog(textEditor) == JFileChooser.APPROVE_OPTION) {
-                //TODO
+                File file = fileChooser.getSelectedFile();
+                PrintStream output;
+                try {
+                    output = new PrintStream(file);
+                    output.print(textArea.getText());
+                } catch (FileNotFoundException e) {
+                    System.out.println("Cannot find file");
+                    return;
+                }
             }
-        }
-        else{
-            //TODO
+        } else {
+            File file = textEditor.currentFile;
+            try (PrintStream output = new PrintStream(file)) {
+                output.print(textArea.getText());
+            } catch (FileNotFoundException e) {
+                System.out.println("Cannot save the file");
+            }
         }
     }
 
     public static void newFile(TextEditor textEditor, JTextArea textArea) {
-        //TODO
+        textArea.setText("");
+        textEditor.currentFile = null;
     }
 }
